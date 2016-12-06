5b46875216dfe2564bca65ae3701e0e1cbfd9676
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2016-12-06 11:51:50 +0900

    新生ex3です。

--- src/main/jp/ac/uryukyu/ie/e165749/ExMain.java
+++ src/main/jp/ac/uryukyu/ie/e165749/ExMain.java
@@ -4,5 +4,23 @@ package jp.ac.uryukyu.ie.e165749;
  * Created by South on 2016/12/06.
  */
 public class ExMain {
+
+    public static void main(){
+        Map map = new Map();
+        Player player = new Player();
+
+        
+        System.out.println("player's point = " + player.getplayer());//playerはオブジェクトの名前。芋でもいいよ。
+
+        x -= 1;
+    }
+
+
+
+
 }
 //仕方ないなああ
+
+//class Map
+//class Player があると仮定
+
