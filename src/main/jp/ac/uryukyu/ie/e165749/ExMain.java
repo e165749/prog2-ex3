1359eb4f63158ff67a6f57e2048505e3322ba296
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2016-12-13 11:53:05 +0900

    Ex_3-3

--- src/main/jp/ac/uryukyu/ie/e165749/ExMain.java
+++ src/main/jp/ac/uryukyu/ie/e165749/ExMain.java
@@ -1,23 +1,37 @@
 package jp.ac.uryukyu.ie.e165749;
 
+import java.io.File;
 /**
+ *
  * Created by South on 2016/12/06.
  */
 public class ExMain {
 
-    public static void main(){
-        Map map = new Map();
-        Player player = new Player();
+    public static void main(String[] args){
+    FileReader();
 
-        
-        System.out.println("player's point = " + player.getplayer());//playerはオブジェクトの名前。芋でもいいよ。
 
-        x -= 1;
     }
 
 
 
+}
+class FileReader(File file){
+    public static void filereader(){
 
+        try{
+        File file = new File("South/IdeaProjects/prog2_ex3/src/main/map.txt");
+        FileReader filereader = new FileReader(file);
+
+        int ch = filereader.read();
+        System.out.println((char)ch);
+        }catch(FileNotFoundException e){
+        System.out.println(e);
+        }catch(IOException e){
+        System.out.println(e);
+        }
+
+        }
 }
 //仕方ないなああ
 
